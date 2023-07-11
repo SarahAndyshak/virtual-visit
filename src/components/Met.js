034eb165f3d44f0b3aee2762f014ef078b6b30d0
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-07-10 18:12:35 -0700

    Remove target=_blank attribute from Met.js - phishing attacks?

--- src/components/Met.js
+++ src/components/Met.js
@@ -9,71 +9,71 @@ function Met() {
       <h1>The Metropolitan Museum of Art, NYC</h1>
       {/* include smooth scrolling between sections? possibly subdirectory to sections */}
 
-      {/* Be sure all links open to new tab */}
+      {/* Should links open to new tab? */}
 
-      <p>The Metropolitan Museum of Art, founded in 1870, houses over 5,000 works of art in two locations: on 5th Avenue and at the Cloisters. The museum has an enormous amount of material available online, including virtual walk-throughs created with <a href="https://artsandculture.google.com/" target="_blank">Google Arts & Culture,</a> digitized works of art, educational resources, essays, and virtual exhibitions.</p>
+      <p>The Metropolitan Museum of Art, founded in 1870, houses over 5,000 works of art in two locations: on 5th Avenue and at the Cloisters. The museum has an enormous amount of material available online, including virtual walk-throughs created with <a href="https://artsandculture.google.com/">Google Arts & Culture,</a> digitized works of art, educational resources, essays, and virtual exhibitions.</p>
 
-      <p>Much of the Met's collection is in the public domain, so visitors can download and use certain images, materials, and data for their own use in accordance with the museum's <a href="https://www.metmuseum.org/about-the-met/policies-and-documents/image-resources" target="_blank">Open Access policy.</a></p>
+      <p>Much of the Met's collection is in the public domain, so visitors can download and use certain images, materials, and data for their own use in accordance with the museum's <a href="https://www.metmuseum.org/about-the-met/policies-and-documents/image-resources">Open Access policy.</a></p>
       {/* add a tooltip definition of public domain */}
 
       <h2>Downloadable Art</h2>
       {/* Create slideshow */}
-      <p>You can search the <a href="https://www.metmuseum.org/art/the-collection" target="_blank">Met's collection</a> by title, keyword, artist, geographic region, historical period, material, and more, or you can look through curated collections available on the museum's site. <a href="https://www.metmuseum.org/art/collection/search?showOnly=highlights%7CwithImage&pageSize=0&sortBy=Relevance&sortOrder=asc&searchField=All" target="_blank">Highlights of the collection</a> have their own page, as do <a href="https://www.metmuseum.org/art/collection/search?showOnly=withImage%7CopenAccess&pageSize=0&sortBy=Relevance&sortOrder=asc&searchField=All" target="_blank">Open Access works</a> available for free download. Click on any of the works and you can learn much more about each piece in depth.</p>
+      <p>You can search the <a href="https://www.metmuseum.org/art/the-collection">Met's collection</a> by title, keyword, artist, geographic region, historical period, material, and more, or you can look through curated collections available on the museum's site. <a href="https://www.metmuseum.org/art/collection/search?showOnly=highlights%7CwithImage&pageSize=0&sortBy=Relevance&sortOrder=asc&searchField=All">Highlights of the collection</a> have their own page, as do <a href="https://www.metmuseum.org/art/collection/search?showOnly=withImage%7CopenAccess&pageSize=0&sortBy=Relevance&sortOrder=asc&searchField=All">Open Access works</a> available for free download. Click on any of the works and you can learn much more about each piece in depth.</p>
 
       <h2>3D Printing Models</h2>
-      <p>Want a copy of a sculptural work from the Met's collection? If you have access to a 3D printer, you can download files for free and print your own copy! In fact, the Met encourages visitors to create their own files for 3D models by photographing works in the public domain. You can download files from the Met's <a href="https://www.thingiverse.com/met/designs" target="_blank">thingiverse page</a> and search for more files created by visitors online.</p>
+      <p>Want a copy of a sculptural work from the Met's collection? If you have access to a 3D printer, you can download files for free and print your own copy! In fact, the Met encourages visitors to create their own files for 3D models by photographing works in the public domain. You can download files from the Met's <a href="https://www.thingiverse.com/met/designs">thingiverse page</a> and search for more files created by visitors online.</p>
 
       <h2>Online Exhibitions</h2>
       <p>Google Arts & Culture hosts several online exhibitions created by the Met. These are just a few samples, check out more on the site!</p>
       {/* add linked image for each, colored background */}
 
       <h3>Making the Met</h3>
-      <a href ="https://artsandculture.google.com/story/2wXR-pBlvVqAJQ" target="_blank"></a>
+      <a href ="https://artsandculture.google.com/story/2wXR-pBlvVqAJQ"></a>
       <p>A multimedia exhibition celebrating the Met's 150th anniversary by recounting its history and showcasing highlights from its collection.</p>
 
       <h3>Pieter Bruegel's Harvesters</h3>
-      <a href="https://artsandculture.google.com/story/kQUxmAo9GNvEJA" target="_blank"></a>
+      <a href="https://artsandculture.google.com/story/kQUxmAo9GNvEJA"></a>
       <p>A closer look at the tiny details in one of Pieter Bruegel's paintings.</p>
       {/* Add a link to the BBC's related radio program */}
       <p>This piece is also featured in an episode of "Moving Pictures," created by BBC Radio 4</p>
-      <a href="https://www.bbc.co.uk/programmes/b07w9jg5" target="_blank"></a>
+      <a href="https://www.bbc.co.uk/programmes/b07w9jg5"></a>
 
       <h3>Before Yesterday We COuld Fly: An Afrofuturist Period Room</h3>
       <p>Discover an installation work inspired by historical Seneca Village, which imagines what a future home could have looked like if the African American community had not been destroyed.</p>
-      <a href="https://artsandculture.google.com/story/OwVRKgZz3RFkSg" target="_blank"></a>
+      <a href="https://artsandculture.google.com/story/OwVRKgZz3RFkSg"></a>
 
       <h3>Behind the Scenes at The Costume Institute Conservation Lab</h3>
       <p>A look at how the historical clothing in the Met's collections are conserved and displayed.</p>
-      <a href="https://artsandculture.google.com/story/ygURqUmRX42zJg" target="_blank"></a>
+      <a href="https://artsandculture.google.com/story/ygURqUmRX42zJg"></a>
 
       <h3>Celebrating Asian and Pacific Islander Heritage</h3>
       <p>This exhibition highlights a few artistic contributions from AAPI artists featured in the Met's collection.</p>
-      <a href="https://artsandculture.google.com/story/ggXBT3SK-gKWqA" target="_blank"></a>
+      <a href="https://artsandculture.google.com/story/ggXBT3SK-gKWqA"></a>
 
       <h2>Virtual Walk-throughs</h2>
       {/* Side-by-side images and links, hover enlarge/color */}
       <p>Google Arts & Culture presents a way to visit both the Met on Fifth Avenue and the Cloisters from anywhere you have internet!</p>
       
       <p>The Met Fifth Avenue houses art from all over the world and many periods of history. Some sections of the museum feature entire buildings, like the Roman-era Temple of Dendur in the Egyptian wing.</p>
-      <a href="https://artsandculture.google.com/streetview/metropolitan-museum-of-art/KAFHmsOTE-4Xyw?sv_lng=-73.9624786&sv_lat=40.7803959&sv_h=322.9&sv_p=0&sv_pid=KeFx8oXHzeuY8L5rfepHaA&sv_z=1" target="_blank">The Met Fifth Avenue</a>
+      <a href="https://artsandculture.google.com/streetview/metropolitan-museum-of-art/KAFHmsOTE-4Xyw?sv_lng=-73.9624786&sv_lat=40.7803959&sv_h=322.9&sv_p=0&sv_pid=KeFx8oXHzeuY8L5rfepHaA&sv_z=1">The Met Fifth Avenue</a>
       
       <p>The Cloisters, which opened in 1938, houses much of the Met's medieval European collection. The Cloisters takes its name from the elements of medieval cloisters incorported into its structure.</p>
       {/* Add tooltip defining cloister */}
-      <a href="https://artsandculture.google.com/streetview/the-cloisters-museum-and-gardens/aAFFBxRsY3TilQ" target="_blank">The Cloisters</a>
+      <a href="https://artsandculture.google.com/streetview/the-cloisters-museum-and-gardens/aAFFBxRsY3TilQ">The Cloisters</a>
 
       <h2>MetPublications</h2>
-      <p>Visitors can learn more about recent publications on <a href ="https://www.metmuseum.org/art/metpublications" target="_blank">this part of the museum's site.</a> Many books, articles, magazines, and exhibition catalogues are available for free download or to be read online.</p>
+      <p>Visitors can learn more about recent publications on <a href ="https://www.metmuseum.org/art/metpublications">this part of the museum's site.</a> Many books, articles, magazines, and exhibition catalogues are available for free download or to be read online.</p>
 
       <h2>The Heilbrunn Timeline of Art History</h2>
       <h3>Artworks | Timelines | Essays</h3>
-      <p><a href="https://www.metmuseum.org/toah" target="_blank">The Heilbrunn Timline of Art History</a> provides another way for visitors to easily <a href="https://www.metmuseum.org/toah/works" target="_blank">find artworks by period, theme, and material,</a> but it also presents <a href="https://www.metmuseum.org/toah/chronology" target="_blank">timelines</a> to help visitors visualize connections. Scholars connected to the museum have <a href="https://www.metmuseum.org/toah/essays" target="_blank">written essays</a> about many specific works of art, as well as movements, themes, and time periods, all available on the Timeline.</p>
+      <p><a href="https://www.metmuseum.org/toah">The Heilbrunn Timline of Art History</a> provides another way for visitors to easily <a href="https://www.metmuseum.org/toah/works">find artworks by period, theme, and material,</a> but it also presents <a href="https://www.metmuseum.org/toah/chronology">timelines</a> to help visitors visualize connections. Scholars connected to the museum have <a href="https://www.metmuseum.org/toah/essays">written essays</a> about many specific works of art, as well as movements, themes, and time periods, all available on the Timeline.</p>
 
       <h2>The Met on Google Arts & Culture</h2>
       <h3>The Met Fifth Avenue | The Cloisters</h3>
       <p>Each of the Met's locations has a dedicated page on Google Arts & Culture where visitors can find the virtual walk-throughs, highlights from the collection, and online exhibitions.</p>
       {/* replace with linked images, spin from image to text */}
-      <a href="https://artsandculture.google.com/partner/the-metropolitan-museum-of-art" target="_blank"><h3>The Met Fifth Avenue</h3></a>
-      <a href="https://artsandculture.google.com/partner/the-cloisters-museum-and-gardens" target="_blank"><h3>The Cloisters</h3></a>
+      <a href="https://artsandculture.google.com/partner/the-metropolitan-museum-of-art"><h3>The Met Fifth Avenue</h3></a>
+      <a href="https://artsandculture.google.com/partner/the-cloisters-museum-and-gardens"><h3>The Cloisters</h3></a>
     </>
     // replace with </StyledMet>
   )
