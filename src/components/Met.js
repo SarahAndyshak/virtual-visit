a3bfbe34bc99bf3c4693e4ae5944f2868883a372
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-08-14 17:04:54 -0700

    Add background image and mask to Online Exhibitions section; begin setting up side-by-side images and info for Virtual Walk-throughs section

--- src/components/Met.js
+++ src/components/Met.js
@@ -28,9 +28,9 @@ function Met() {
       <div className="bg-red-500 p-6 text-white">
         <h1 className="text-center text-white">The Metropolitan Museum of Art, NYC</h1>
 
-        <p>The Metropolitan Museum of Art, founded in 1870, houses over 5,000 works of art in two locations: on 5th Avenue and at the Cloisters. The museum has an enormous amount of material available online, including virtual walk-throughs created with <a href="https://artsandculture.google.com/">Google Arts & Culture,</a> digitized works of art, educational resources, essays, and virtual exhibitions.</p>
+        <p>The Metropolitan Museum of Art, founded in 1870, houses over 5,000 works of art in two locations: on 5th Avenue and at the Cloisters. The museum has an enormous amount of material available online, including virtual walk-throughs created with <a className="text-white" href="https://artsandculture.google.com/">Google Arts & Culture,</a> digitized works of art, educational resources, essays, and virtual exhibitions.</p>
 
-        <p>Much of the Met's collection is in the public domain, so visitors can download and use certain images, materials, and data for their own use in accordance with the museum's <a href="https://www.metmuseum.org/about-the-met/policies-and-documents/image-resources">Open Access policy.</a></p>
+        <p>Much of the Met's collection is in the public domain, so visitors can download and use certain images, materials, and data for their own use in accordance with the museum's <a className="text-white" href="https://www.metmuseum.org/about-the-met/policies-and-documents/image-resources">Open Access policy.</a></p>
       {/* add a tooltip definition of public domain */}
       </div>
 
@@ -47,65 +47,83 @@ function Met() {
 
 {/* Create slideshow here? */}
 
-      <div className="inline-block pr-6">
+      <div className="inline-block">
         <img src={Oxbow} alt="Oxbow" className="mr-6 md:w-1/2 float-left" />
-
+        
+        <div className="mr-6 ml-6">
         <h2>Downloadable Art</h2>
         <p>You can search the <a href="https://www.metmuseum.org/art/the-collection">Met's collection</a> by title, keyword, artist, geographic region, historical period, material, and more, or you can look through curated collections available on the museum's site. <a href="https://www.metmuseum.org/art/collection/search?showOnly=highlights%7CwithImage&pageSize=0&sortBy=Relevance&sortOrder=asc&searchField=All">Highlights of the collection</a> have their own page, as do <a href="https://www.metmuseum.org/art/collection/search?showOnly=withImage%7CopenAccess&pageSize=0&sortBy=Relevance&sortOrder=asc&searchField=All">Open Access works</a> available for free download. Click on any of the works and you can learn much more about each piece in depth.</p>
       
         <h2>3D Printing Models</h2>
         <p>Want a copy of a sculptural work from the Met's collection? If you have access to a 3D printer, you can download files for free and print your own copy! In fact, the Met encourages visitors to create their own files for 3D models by photographing works in the public domain. You can download files from the Met's <a href="https://www.thingiverse.com/met/designs">thingiverse page</a> and search for more files created by visitors online.</p>
+        </div>
+
       </div>
+
+
+{/* Try turning this section into a carousel once the page is set up */}
 
       <div
-        className="w-full p-12 bg-no-repeat bg-cover text-center text-white"
-        style={{backgroundImage: `url(${WaveDetail})`}}
+        className="w-full bg-no-repeat bg-cover bg-center"
+        style={{backgroundImage: `url(${Window})`}}
         >
+        <div class="bg-[hsla(0,0%,0%,0.7)]">
+        <div class="flex justify-center items-center h-full">
 
+        <div className="text-center text-white p-6">
         <h2 className="text-center text-white">Online Exhibitions</h2>
         <h3 className="text-center text-white">Google Arts & Culture hosts several online exhibitions created by the Met. These are just a few samples, check out more on the site!</h3>
-        
-        <h3>Making the Met</h3>
-        <a href ="https://artsandculture.google.com/story/2wXR-pBlvVqAJQ"></a>
+
+        <h3><a className="text-center text-white" href="https://artsandculture.google.com/story/2wXR-pBlvVqAJQ">Making the Met</a></h3>
         <p>A multimedia exhibition celebrating the Met's 150th anniversary by recounting its history and showcasing highlights from its collection.</p>
         
-        <h3>Behind the Scenes at The Costume Institute Conservation Lab</h3>
+        <h3><a className="text-center text-white" href="https://artsandculture.google.com/story/ygURqUmRX42zJg">Behind the Scenes at The Costume Institute Conservation Lab</a></h3>
         <p>A look at how the historical clothing in the Met's collections are conserved and displayed.</p>
-        <a href="https://artsandculture.google.com/story/ygURqUmRX42zJg"></a>
 
-        <h3>Celebrating Asian and Pacific Islander Heritage</h3>
+        <h3><a className="text-center text-white" href="https://artsandculture.google.com/story/ggXBT3SK-gKWqA">Celebrating Asian and Pacific Islander Heritage</a></h3>
         <p>This exhibition highlights a few artistic contributions from AAPI artists featured in the Met's collection.</p>
-        <a href="https://artsandculture.google.com/story/ggXBT3SK-gKWqA"></a>
         
-        <h3>Before Yesterday We Could Fly: An Afrofuturist Period Room</h3>
+        <h3><a className="text-center text-white" href="https://artsandculture.google.com/story/OwVRKgZz3RFkSg">Before Yesterday We Could Fly: An Afrofuturist Period Room</a></h3>
         <p>Discover an installation work inspired by historical Seneca Village, which imagines what a future home could have looked like if the African American community had not been destroyed.</p>
-        <a href="https://artsandculture.google.com/story/OwVRKgZz3RFkSg"></a>
       
-        <h3>Pieter Bruegel's Harvesters</h3>
-        <a href="https://artsandculture.google.com/story/kQUxmAo9GNvEJA"></a>
-        <p>A closer look at the tiny details in one of Pieter Bruegel's paintings.</p>
-        {/* Add a link to the BBC's related radio program */}
-        <p>This piece is also featured in an episode of "Moving Pictures," created by BBC Radio 4</p>
-        <a href="https://www.bbc.co.uk/programmes/b07w9jg5"></a>
+        <h3><a className="text-center text-white" href="https://artsandculture.google.com/story/kQUxmAo9GNvEJA">Pieter Bruegel's Harvesters</a></h3>
+        <p>A closer look at the tiny details in one of Pieter Bruegel's paintings. This piece is also featured in <a className="text-white" href="https://www.bbc.co.uk/programmes/b07w9jg5">an episode of "Moving Pictures,"</a> created by BBC Radio 4</p>
+
+        </div>
+        </div>
+        </div>
+
       </div>
 
 
-      {/* add linked image for each, colored background */}
 
-
-
-
-
+      <div className="text-center">
       <h2>Virtual Walk-throughs</h2>
       {/* Side-by-side images and links, hover enlarge/color */}
+      
+{/* Probably need to make two containers, put the images inside and float them */}
+
       <p>Google Arts & Culture presents a way to visit both the Met on Fifth Avenue and the Cloisters from anywhere you have internet!</p>
-      
-      <p>The Met Fifth Avenue houses art from all over the world and many periods of history. Some sections of the museum feature entire buildings, like the Roman-era Temple of Dendur in the Egyptian wing.</p>
+      </div>
+
       <a href="https://artsandculture.google.com/streetview/metropolitan-museum-of-art/KAFHmsOTE-4Xyw?sv_lng=-73.9624786&sv_lat=40.7803959&sv_h=322.9&sv_p=0&sv_pid=KeFx8oXHzeuY8L5rfepHaA&sv_z=1">The Met Fifth Avenue</a>
-      
+      <p>The Met Fifth Avenue houses art from all over the world and many periods of history. Some sections of the museum feature entire buildings, like the Roman-era Temple of Dendur in the Egyptian wing.</p>
+
+      <a href="https://artsandculture.google.com/streetview/the-cloisters-museum-and-gardens/aAFFBxRsY3TilQ">The Cloisters</a>
       <p>The Cloisters, which opened in 1938, houses much of the Met's medieval European collection. The Cloisters takes its name from the elements of medieval cloisters incorported into its structure.</p>
       {/* Add tooltip defining cloister */}
-      <a href="https://artsandculture.google.com/streetview/the-cloisters-museum-and-gardens/aAFFBxRsY3TilQ">The Cloisters</a>
+
+      <div className="inline-block">
+        <img src={Dancer} alt="Mastaba tomb" className="float-left h-screen" />
+
+      </div>
+      <div className="inline-block">
+        <img src={Book} alt="Book cover" className="float-right h-screen"/>
+      </div>
+
+
+      
+      
 
       <h2>MetPublications</h2>
       <p>Visitors can learn more about recent publications on <a href ="https://www.metmuseum.org/art/metpublications">this part of the museum's site.</a> Many books, articles, magazines, and exhibition catalogues are available for free download or to be read online.</p>
