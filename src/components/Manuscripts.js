5bedbbbc8658bd007f33cf5fb41a30a487f48881
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-08-31 14:46:15 -0700

    Make button height responsive

--- src/components/Manuscripts.js
+++ src/components/Manuscripts.js
@@ -48,7 +48,7 @@ function Manuscripts() {
 
       <div className="py-44 px-12 text-center text-white">
         <div>
-          <a href="https://digitalcollections.tcd.ie/concern/works/hm50tr726?locale=en"><Button className="bg-cyan-900 m-1 hover:bg-cyan-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">The Book of Kells</Button></a>
+          <a href="https://digitalcollections.tcd.ie/concern/works/hm50tr726?locale=en"><Button className="bg-cyan-900 h-fit hover:bg-cyan-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">The Book of Kells</Button></a>
         </div>
 
       </div>
@@ -70,27 +70,11 @@ function Manuscripts() {
       
       <div className="bg-lime-100 p-6 py-12 text-center md:w-2/3">
       
-        <a href="https://manuscripts.thewalters.org/"><Button className="bg-green-700 hover:bg-green-500 text-2xl text-yellow-100 border-0">The Walters Ex Libris</Button></a>
+        <a href="https://manuscripts.thewalters.org/"><Button className="bg-green-700 h-fit hover:bg-green-500 text-2xl text-yellow-100 border-0">The Walters Ex Libris</Button></a>
         <p><a href="https://thewalters.org/">The Walters Art Museum</a> in Baltimore has created a fun interface where you can flip through the pages of their digitized manuscripts. Search their collection by book type, period, culture, region, and more!</p>
       </div>
     </div>
 
-
-{/* MetPublications */}
-    {/* <div className="bg-green-700 p-1"></div>
-
-    <img 
-      src={BookPlateDetail}
-      alt="Book Plate fromt the Metropolitan Museum of Art's collection"
-      className="w-screen my-0"
-    />
-    <div className="text-center p-6">
-      <a href="https://www.metmuseum.org/art/metpublications"><Button className="bg-red-700 hover:bg-red-500 text-2xl text-white border-0">Met Publications</Button></a>
-
-      <p>Books, magazines, and exhibition catalogues related to the <a href="https://www.metmuseum.org/">Metropolitan Museum of Art's</a> collection, available to download or read online.</p>
-    </div>
-
-    <div className="bg-sky-700 p-1"></div> */}
 
 {/* MetPublications 2 */}
     <div
@@ -102,7 +86,7 @@ function Manuscripts() {
 
       <div className="py-44 px-12 text-center text-white">
         <div className="text-center p-6">
-        <a href="https://www.metmuseum.org/art/metpublications"><Button className="bg-red-700 hover:bg-red-500 text-2xl text-white border-0">Met Publications</Button></a>
+        <a href="https://www.metmuseum.org/art/metpublications"><Button className="bg-red-700 h-fit hover:bg-red-500 text-2xl text-white border-0">Met Publications</Button></a>
 
         <p>Books, magazines, and exhibition catalogues related to the <a className="text-white hover:text-red-500" href="https://www.metmuseum.org/">Metropolitan Museum of Art's</a> collection, available to download or read online.</p>
         </div>
@@ -123,11 +107,11 @@ function Manuscripts() {
         <p><a href="https://www.getty.edu/">The Getty</a> in Los Angeles has digitized many of the manuscripts in its collection. Many of these images are in the public domain and open for creative use. You can also read many of their publications online.</p>
 
       {/* Buttons */}
-        <a href="getty.edu/art/manuscripts/"><Button className="bg-sky-700 m-1 hover:bg-sky-500 text-2xl text-sky-100 border-0">Highlights from the Manuscript Collection</Button></a>
+        <a href="getty.edu/art/manuscripts/"><Button className="bg-sky-700 m-1 h-fit hover:bg-sky-500 text-2xl text-sky-100 border-0">Highlights from the Manuscript Collection</Button></a>
 
-        <a href="https://www.getty.edu/art/collection/search?department=Manuscripts"><Button className="bg-sky-700 m-1 hover:bg-sky-500 text-2xl text-sky-100 border-0">Digitized Manuscripts</Button></a>
+        <a href="https://www.getty.edu/art/collection/search?department=Manuscripts"><Button className="bg-sky-700 m-1 h-fit hover:bg-sky-500 text-2xl text-sky-100 border-0">Digitized Manuscripts</Button></a>
 
-        <a href="https://www.getty.edu/research/publications/electronic_publications/"><Button className="bg-sky-700 m-1 hover:bg-sky-500 text-2xl text-sky-100 border-0">Digital Books</Button></a>
+        <a href="https://www.getty.edu/research/publications/electronic_publications/"><Button className="bg-sky-700 m-1 h-fit hover:bg-sky-500 text-2xl text-sky-100 border-0">Digital Books</Button></a>
       </div>
         <img 
           src={MiraDE} 
@@ -145,19 +129,19 @@ function Manuscripts() {
       <p>London's <a className="text-white" href="https://www.bl.uk/">British Library</a> has numerous collections, catalogues, and exhibitions on their site. Here are a few ways to start exploring!</p>
       
     {/* button block */}
-      <a href="https://www.bl.uk/catalogues-and-collections/digital-collections"><Button className="bg-white m-1 hover:bg-red-200 text-xl text-red-700 border-0">All Digital Collections</Button></a>
+      <a href="https://www.bl.uk/catalogues-and-collections/digital-collections"><Button className="bg-white m-1 h-fit hover:bg-red-200 text-xl text-red-700 border-0">All Digital Collections</Button></a>
 
-      <a href="https://www.bl.uk/onlinegallery/ttp/accessiblettp.html"><Button className="bg-white m-1 hover:bg-red-200 text-xl text-red-700 border-0">Turning the Pages</Button></a>
+      <a href="https://www.bl.uk/onlinegallery/ttp/accessiblettp.html"><Button className="bg-white m-1 h-fit hover:bg-red-200 text-xl text-red-700 border-0">Turning the Pages</Button></a>
 
-      <a href="https://www.bl.uk/manuscripts/"><Button className="bg-white m-1 hover:bg-red-200 text-xl text-red-700 border-0">Digitised Manuscripts</Button></a>
+      <a href="https://www.bl.uk/manuscripts/"><Button className="bg-white m-1 h-fit hover:bg-red-200 text-xl text-red-700 border-0">Digitised Manuscripts</Button></a>
 
-      <a href="https://www.bl.uk/catalogues/illuminatedmanuscripts/welcome.htm"><Button className="bg-white m-1 hover:bg-red-200 text-xl text-red-700 border-0">Catalogue of Illuminated Manuscripts</Button></a>
+      <a href="https://www.bl.uk/catalogues/illuminatedmanuscripts/welcome.htm"><Button className="bg-white m-1 h-fit hover:bg-red-200 text-xl text-red-700 border-0">Catalogue of Illuminated Manuscripts</Button></a>
 
-      <a href="https://sounds.bl.uk/"><Button className="bg-white m-1 hover:bg-red-200 text-xl text-red-700 border-0">Sounds</Button></a>
+      <a href="https://sounds.bl.uk/"><Button className="bg-white m-1 h-fit hover:bg-red-200 text-xl text-red-700 border-0">Sounds</Button></a>
 
-      <a href="https://www.bl.uk/treasures-in-full"><Button className="bg-white m-1 hover:bg-red-200 text-xl text-red-700 border-0">Treasures in Full</Button></a>
+      <a href="https://www.bl.uk/treasures-in-full"><Button className="bg-white m-1 h-fit hover:bg-red-200 text-xl text-red-700 border-0">Treasures in Full</Button></a>
 
-      <a href="https://www.bl.uk/discover-and-learn/online-exhibitions"><Button className="bg-white m-1 hover:bg-red-200 text-xl text-red-700 border-0">Online Exhibitions</Button></a>
+      <a href="https://www.bl.uk/discover-and-learn/online-exhibitions"><Button className="bg-white m-1 h-fit hover:bg-red-200 text-xl text-red-700 border-0">Online Exhibitions</Button></a>
 
     </div> 
 
@@ -176,11 +160,11 @@ function Manuscripts() {
 
         <p>The <a href="https://www.themorgan.org/">Morgan Library and Museum</a> (New York City) provides visitors with commentary on many works in their collection, along with zoomable images.</p>
         
-        <a href="https://www.themorgan.org/digital-facsimiles"><Button className="bg-yellow-700 m-1 hover:bg-yellow-500 text-2xl text-amber-100 border-0">Digital Facsimiles</Button></a>
+        <a href="https://www.themorgan.org/digital-facsimiles"><Button className="bg-yellow-700 m-1 h-fit hover:bg-yellow-500 text-2xl text-amber-100 border-0">Digital Facsimiles</Button></a>
 
-        <a href="https://www.themorgan.org/online-exhibitions"><Button className="bg-yellow-700 m-1 hover:bg-yellow-500 text-2xl text-amber-100 border-0">Online Exhibitions</Button></a>
+        <a href="https://www.themorgan.org/online-exhibitions"><Button className="bg-yellow-700 m-1 h-fit hover:bg-yellow-500 text-2xl text-amber-100 border-0">Online Exhibitions</Button></a>
 
-        <a href="https://www.themorgan.org/collection"><Button className="bg-yellow-700 m-1 hover:bg-yellow-500 text-2xl text-amber-100 border-0">Explore the Collection</Button></a>
+        <a href="https://www.themorgan.org/collection"><Button className="bg-yellow-700 m-1 h-fit hover:bg-yellow-500 text-2xl text-amber-100 border-0">Explore the Collection</Button></a>
 
       </div>
     </div>
@@ -196,7 +180,7 @@ function Manuscripts() {
 
       <div className="text-center text-white p-6">
         
-      <a href="https://www.getty.edu/research/publications/electronic_publications/"><Button className="bg-transparent border-white-2 hover:bg-yellow-500 text-2xl text-white">The World Digital Library</Button></a>
+      <a href="https://www.getty.edu/research/publications/electronic_publications/"><Button className="bg-transparent border-white-2 h-fit hover:bg-yellow-500 text-2xl text-white">The World Digital Library</Button></a>
 
         {/* <h1 className="text-white">The World Digital Library</h1> */}
 
@@ -214,7 +198,7 @@ function Manuscripts() {
       
       <div className="bg-lime-100 p-6 py-12 text-center md:w-2/3">
 
-        <a href="https://gallica.bnf.fr/accueil/en/content/accueil-en?mode=desktop"><Button className="bg-cyan-900 m-1 hover:bg-cyan-700 text-2xl text-lime-100 border-0 ">Gallica</Button></a>
+        <a href="https://gallica.bnf.fr/accueil/en/content/accueil-en?mode=desktop"><Button className="bg-cyan-900 h-fit hover:bg-cyan-700 text-2xl text-lime-100 border-0 ">Gallica</Button></a>
 
         <p><a href="https://gallica.bnf.fr/accueil/en/content/accueil-en?mode=desktop">Gallica</a> is one of the digital libraries connected with the Bibliothèque nationale de France. Its resources are largely in the public domain. Visit this site for digitized manuscripts, incunabula, books, magazines, photographs, and music housed in the BnF's collection.</p>
       </div>
