b8e6bc1b809064a9ce938eafe80d4f5fe81e008c
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-09-04 17:45:51 -0700

    Update carousel with navigation buttons, still not perfect, but clearer.

--- src/components/Getty.js
+++ src/components/Getty.js
@@ -22,46 +22,102 @@ function Getty() {
     <>
     <article className="prose max-w-none">
 
+{/* Carousel 1 test */}
+{/* <div className="flex justify-center">
+  <Carousel />
+</div> */}
+
     <div className="bg-blue-900 text-center p-6">
       <h1 className="text-white">Getty: Conservation • Research • Foundation • Museum</h1>
-      <h2 className="text-white">Swipe through the images using your touchpad or finger, then check out the resources below!</h2>
+      <h2 className="text-white">Take at a look at a few of the Getty's works in the slideshow, then check out the resources below!</h2>
     </div>
 
-    {/* <div className="flex justify-center">
-      <Carousel />
-    </div> */}
+{/* Slide navigation buttons */}
+      {/* <div className="flex justify-center w-full py-2 gap-2">
+        <a href="#item1" className="btn btn-xs">1</a> 
+        <a href="#item2" className="btn btn-xs">2</a> 
+        <a href="#item3" className="btn btn-xs">3</a> 
+        <a href="#item4" className="btn btn-xs">4</a>
+        <a href="#item5" className="btn btn-xs">5</a>
+        <a href="#item6" className="btn btn-xs">6</a>
+      </div> */}
 
 
-
-
-    <div className="flex justify-center bg-blue-900 py-0">
-      <div className="carousel rounded-box">
+{/* Carousel */}
+    {/* <div className="flex justify-center bg-blue-900 py-0">
+      <div id="item1" className="carousel rounded-box">
         <div className="carousel-item">
           <img src={CollageGarland} alt="Collage" className="h-screen" />
         </div>
 
-        <div className="carousel-item">
+        <div id="item2" className="carousel-item">
           <img src={StJoseph} alt="Collage" className="h-screen" />
         </div>
 
-        <div className="carousel-item">
+        <div id="item3" className="carousel-item">
           <img src={Mira} alt="Mira" className="h-screen" />
         </div>
 
-        <div className="carousel-item">
+        <div id="item4" className="carousel-item">
           <img src={Parasol} alt="Collage" className="h-screen" />
         </div>
 
-        <div className="carousel-item">
+        <div id="item5" className="carousel-item">
           <img src={CanonTable} alt="Collage" className="h-screen" />
         </div>
 
-        <div className="carousel-item">
+        <div id="item6" className="carousel-item">
           <img src={Kouros} alt="Collage" className="h-screen" />
         </div>
 
       </div>
+    </div> */}
+
+    <div className="carousel h-screen bg-blue-900">
+      <div id="slide1" className="carousel-item relative h-screen">
+        <img src={CollageGarland} className="h-screen" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" className="btn btn-circle">❮</a> 
+          <a href="#slide2" className="btn btn-circle">❯</a>
+        </div>
+      </div> 
+      <div id="slide2" className="carousel-item relative h-screen">
+        <img src={StJoseph} className="h-screen" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" className="btn btn-circle">❮</a> 
+          <a href="#slide3" className="btn btn-circle">❯</a>
+        </div>
+      </div> 
+      <div id="slide3" className="carousel-item relative h-screen">
+        <img src={Mira} className="h-screen" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" className="btn btn-circle">❮</a> 
+          <a href="#slide4" className="btn btn-circle">❯</a>
+        </div>
+      </div> 
+      <div id="slide4" className="carousel-item relative h-screen">
+        <img src={Parasol} className="h-screen" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide3" className="btn btn-circle">❮</a> 
+          <a href="#slide5" className="btn btn-circle">❯</a>
+        </div>
+      </div>
+      <div id="slide5" className="carousel-item relative h-screen">
+        <img src={CanonTable} className="h-screen" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" className="btn btn-circle">❮</a> 
+          <a href="#slide6" className="btn btn-circle">❯</a>
+        </div>
+      </div>
+      <div id="slide6" className="carousel-item relative h-screen">
+        <img src={Kouros} className="h-screen" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide5" className="btn btn-circle">❮</a> 
+          <a href="#slide1" className="btn btn-circle">❯</a>
+        </div>
+      </div>
     </div>
+
 
 {/* Buttons */}
 
