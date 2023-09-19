5e36370ef678413588ce1f296ef8983a1e3a6252
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-09-19 10:48:00 -0700

    Fix placement of buttons on slide images using code from navigation buttons to center.

--- src/components/Exhibitions.js
+++ src/components/Exhibitions.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Button, Card } from 'react-daisyui'
 
-import Mastaba from './../imgs/MetImgs/Mastaba.jpeg'
+import Window from './../imgs/MetImgs/LaFargeWindow.jpeg'
+import CollageGarland from "./../imgs/Getty/CollageGarland.jpg"
+
 
 
 
@@ -10,26 +13,138 @@ function Exhibitions() {
     <>
     <article className="prose max-w-none">
 
+{/* Header */}
     <div className="bg-fuchsia-900 text-center p-6">
       <h1 className="text-white">Online Exhibitions</h1>
       <h2 className="text-white">Many museums around the world have exhibitions available online, both at their own sites, and on Google Arts & Culture. <br /><br />These are just a few to start your explorations.</h2>
     </div>
 
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <figure><img src={Mastaba} alt="Temple of Dendur" /></figure>
-      <div className="card-body">
-        <h2 className="card-title">
-          Shoes!
-          <div className="badge badge-secondary">NEW</div>
-        </h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <div className="badge badge-outline">Fashion</div> 
-          <div className="badge badge-outline">Products</div>
+{/* <Cards */}
+    <div className="grid grid-cols-1 md:grid-cols-3">
+
+<label className="swap">
+  <input type="checkbox" />
+    <div className="swap-on">
+      <div className="bg-no-repeat bg-cover bg-center m-1" style={{backgroundImage: `url(${Window})`}}>
+        <div className="text-center justify items-center">
+          <a href="https://artsandculture.google.com/search/exhibit?q=metropolitan%20museum%20of%20art"><Button className="bg-blue-500 h-fit hover:bg-blue-300">All Exhibitions</Button></a>
         </div>
       </div>
     </div>
 
+    <div className="swap-off">
+      <div className="bg-no-repeat bg-cover bg-center m-1" style={{backgroundImage: `url(${Window})`}}>
+        <div className="text-center justify items-center">
+          <h1 className="text-white">The Metropolitan Museum of Art</h1>
+        </div>
+      </div>
+    </div>
+
+    </label>
+
+    <label className="swap">
+  <input type="checkbox" />
+    <div className="swap-on">
+      <div className="bg-no-repeat bg-cover bg-center text-primary-content m-1" style={{backgroundImage: `url(${CollageGarland})`}}>
+        <div className="p-6 text-center">
+          <a href="https://artsandculture.google.com/search/exhibit?q=metropolitan%20museum%20of%20art"><Button className="bg-blue-500 h-fit hover:bg-blue-300">All Exhibitions</Button></a>
+        </div>
+      </div>
+    </div>
+
+    <div className="swap-off">
+      <div className="bg-no-repeat bg-cover bg-center text-primary-content m-1" style={{backgroundImage: `url(${CollageGarland})`}}>
+        <div className="p-6 text-center">
+          <h1 className="text-white">The Metropolitan Museum of Art</h1>
+        </div>
+      </div>
+    </div>
+
+    </label>
+
+
+      {/* <div className="bg-green-500 m-1">side 1</div>
+
+      <div className="bg-blue-500 m-1">side 1</div>
+
+      <div className="bg-red-500 m-1">side 1</div> */}
+
+
+
+
+
+
+
+      {/* <div className="card bg-no-repeat bg-cover bg-center text-primary-content m-1" style={{backgroundImage: `url(${Window})`}}> */}
+        {/* <div className="bg-[hsla(0,0%,0%,0.3)]">
+        <div className="flex justify-center items-center h-full"> */}
+          {/* <div className="card-body items-center text-center">
+            <h2 className="card-title text-white">The Met</h2>
+              <Link to="/met"><Button onClick={() => window.scrollTo(0,0)} 
+              className="bg-blue-500 h-fit hover:bg-blue-300">Selected Exhibitions</Button></Link>
+              <a href="https://artsandculture.google.com/search/exhibit?q=metropolitan%20museum%20of%20art"><Button className="bg-blue-500 h-fit hover:bg-blue-300">All Exhibitions</Button></a>
+          </div> */}
+        {/* </div>
+        </div> */}
+      {/* </div> */}
+
+{/* Getty */}
+      {/* <div className="card bg-no-repeat bg-cover bg-center text-primary-content m-1" style={{backgroundImage: `url(${CollageGarland})`}}>
+        {/* <div className="bg-[hsla(0,0%,0%,0.3)]">
+        <div className="flex justify-center items-center h-full"> */}
+          {/* <div className="card-body items-center text-center">
+            <h2 className="card-title text-white">The Getty</h2>
+            <a href="https://www.getty.edu/research/exhibitions_events/online_exhibitions.html"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">The Getty</Button></a>
+          </div> */}
+        {/* </div>
+        </div> */}
+      {/* </div> */}
+
+{/* Test Card */}
+      {/* <div className="card bg-red-500 text-primary-content m-1">
+        <div className="card-body">
+          <h2 className="card-title">Card title!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn">Buy Now</button>
+          </div>
+        </div>
+      </div> */}
+
+
+{/* Test Swap Card */}
+    {/* <label className="swap">
+      <input type="checkbox" />
+      <div className="swap-on">
+        <div className="card bg-red-500 text-primary-content m-1">
+          <div className="card-body">
+            <h2 className="card-title">Side 1!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="swap-off">
+        <div className="card bg-blue-500 text-primary-content m-1">
+          <div className="card-body">
+            <h2 className="card-title">Side 2!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </label> */}
+
+
+
+
+    </div>
 
     </article>
     </>
--- src/components/Getty.js
+++ src/components/Getty.js
@@ -32,6 +32,121 @@ function Getty() {
       <h2 className="text-white">Scroll through the slideshow for links to resources!</h2>
     </div>
 
+
+
+
+{/* button carousel */}
+    <div className="carousel w-screen h-screen">
+      <div id="slide1" className="carousel-item relative w-screen">
+        <div
+          className="bg-cover bg-center w-screen"
+          style={{backgroundImage: `url(${Mira})`}}
+        >
+          {/* <div className="py-32 text-center">
+            <div>
+              <a href="https://www.getty.edu/art/collection/search?open_content=true"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Open Access Images</Button></a>
+            </div>
+          </div> */}
+          <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <div>
+              <a href="https://www.getty.edu/art/collection/search?open_content=true"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Open Access Images</Button></a>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide6" className="btn btn-circle">❮</a> 
+          <a href="#slide2" className="btn btn-circle">❯</a>
+        </div>
+      </div> 
+
+      <div id="slide2" className="carousel-item relative w-screen">
+        <div
+          className="bg-cover bg-top w-screen"
+          style={{backgroundImage: `url(${StJoseph})`}}
+        >
+          <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <div>
+              <a href="https://www.getty.edu/research/exhibitions_events/online_exhibitions.html"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Online Exhibitions</Button></a>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" className="btn btn-circle">❮</a> 
+          <a href="#slide3" className="btn btn-circle">❯</a>
+        </div>
+      </div> 
+
+      <div id="slide3" className="carousel-item relative w-screen">
+        <div
+          className="bg-cover bg-top w-screen"
+          style={{backgroundImage: `url(${Parasol})`}}
+        >
+          <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 top-1/2">            
+            <div>
+              <a href="https://www.getty.edu/art/collection/search?department=Manuscripts"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Digitized Manuscripts</Button></a>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" className="btn btn-circle">❮</a> 
+          <a href="#slide4" className="btn btn-circle">❯</a>
+        </div>
+      </div> 
+
+      <div id="slide4" className="carousel-item relative w-screen">
+        <div
+          className="bg-cover bg-top w-screen"
+          style={{backgroundImage: `url(${Kouros})`}}
+        >
+          <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <div>
+              <a href="https://www.getty.edu/podcasts/"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Podcasts</Button></a>
+            </div>
+          </div>
+        </div>        
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide3" className="btn btn-circle">❮</a> 
+          <a href="#slide5" className="btn btn-circle">❯</a>
+        </div>
+      </div>
+
+      <div id="slide5" className="carousel-item relative w-screen">
+        <div
+          className="bg-cover bg-center w-screen"
+          style={{backgroundImage: `url(${CollageGarland})`}}
+        >
+          <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <div>
+              <a href="https://www.getty.edu/art/"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Collection Highlights</Button></a>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" className="btn btn-circle">❮</a> 
+          <a href="#slide6" className="btn btn-circle">❯</a>
+        </div>
+      </div>
+
+      <div id="slide6" className="carousel-item relative w-screen">
+        <div
+          className="bg-cover bg-center w-screen"
+          style={{backgroundImage: `url(${CanonTable})`}}
+        >
+          <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <div>
+              <a href="https://artsandculture.google.com/streetview/the-j-paul-getty-museum/cwFdGYSXlaOg6w?sv_lng=-118.473493&sv_lat=34.0771277&sv_h=-18&sv_p=0&sv_pid=SQiOe6lNRxGuLEN-mwxVdA&sv_z=1"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Virtual Walk-through</Button></a>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide5" className="btn btn-circle">❮</a> 
+          <a href="#slide1" className="btn btn-circle">❯</a>
+        </div>
+      </div>
+
+    </div>
+
+
 {/* Slide navigation buttons */}
       {/* <div className="flex justify-center w-full py-2 gap-2">
         <a href="#item1" className="btn btn-xs">1</a> 
@@ -119,116 +234,6 @@ function Getty() {
         </div>
       </div>
     </div> */}
-
-
-{/* button carousel */}
-    <div className="carousel w-screen h-screen">
-      <div id="slide1" className="carousel-item relative w-screen">
-        <div
-          className="bg-cover bg-center w-screen"
-          style={{backgroundImage: `url(${Mira})`}}
-        >
-          <div className="py-32 text-center">
-            <div>
-              <a href="https://www.getty.edu/art/collection/search?open_content=true"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Open Access Images</Button></a>
-            </div>
-          </div>
-        </div>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide6" className="btn btn-circle">❮</a> 
-          <a href="#slide2" className="btn btn-circle">❯</a>
-        </div>
-      </div> 
-
-      <div id="slide2" className="carousel-item relative w-screen">
-        <div
-          className="bg-cover bg-top w-screen"
-          style={{backgroundImage: `url(${StJoseph})`}}
-        >
-          <div className="py-32 text-center">
-            <div>
-              <a href="https://www.getty.edu/research/exhibitions_events/online_exhibitions.html"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Online Exhibitions</Button></a>
-            </div>
-          </div>
-        </div>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">❮</a> 
-          <a href="#slide3" className="btn btn-circle">❯</a>
-        </div>
-      </div> 
-
-      <div id="slide3" className="carousel-item relative w-screen">
-        <div
-          className="bg-cover bg-top w-screen"
-          style={{backgroundImage: `url(${Parasol})`}}
-        >
-          <div className="py-32 text-center">
-            <div>
-              <a href="https://www.getty.edu/art/collection/search?department=Manuscripts"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Digitized Manuscripts</Button></a>
-            </div>
-          </div>
-        </div>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">❮</a> 
-          <a href="#slide4" className="btn btn-circle">❯</a>
-        </div>
-      </div> 
-
-      <div id="slide4" className="carousel-item relative w-screen">
-        <div
-          className="bg-cover bg-top w-screen"
-          style={{backgroundImage: `url(${Kouros})`}}
-        >
-          <div className="py-32 text-center">
-            <div>
-              <a href="https://www.getty.edu/podcasts/"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Podcasts</Button></a>
-            </div>
-          </div>
-        </div>        
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">❮</a> 
-          <a href="#slide5" className="btn btn-circle">❯</a>
-        </div>
-      </div>
-
-      <div id="slide5" className="carousel-item relative w-screen">
-        <div
-          className="bg-cover bg-center w-screen"
-          style={{backgroundImage: `url(${CollageGarland})`}}
-        >
-          <div className="py-32 px-12 text-center text-white">
-            <div>
-              <a href="https://www.getty.edu/art/"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Collection Highlights</Button></a>
-            </div>
-          </div>
-        </div>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle">❮</a> 
-          <a href="#slide6" className="btn btn-circle">❯</a>
-        </div>
-      </div>
-
-      <div id="slide6" className="carousel-item relative w-screen">
-        <div
-          className="bg-cover bg-center w-screen"
-          style={{backgroundImage: `url(${CanonTable})`}}
-        >
-          <div className="py-32 px-12 text-center text-white">
-            <div>
-              <a href="https://artsandculture.google.com/streetview/the-j-paul-getty-museum/cwFdGYSXlaOg6w?sv_lng=-118.473493&sv_lat=34.0771277&sv_h=-18&sv_p=0&sv_pid=SQiOe6lNRxGuLEN-mwxVdA&sv_z=1"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Virtual Walk-through</Button></a>
-            </div>
-          </div>
-        </div>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide5" className="btn btn-circle">❮</a> 
-          <a href="#slide1" className="btn btn-circle">❯</a>
-        </div>
-      </div>
-
-    </div>
-
-
-
 
 {/* Buttons */}
 
