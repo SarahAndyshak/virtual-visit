eafcf46ab2e137781cfb516a0a58e45ba16ab4bc
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-09-21 14:56:35 -0700

    Update image credits in README and ImageCredits, rearrange Footer and update link to ImageCredits

--- src/components/ImageCredits.js
+++ src/components/ImageCredits.js
@@ -9,42 +9,117 @@ function ImageCredits() {
       
       <hr />
       
+      <div className="mx-12">
+
+{/* Splash Page */}
       <div>
         <h2>Background images for the Home Page</h2>
+
         <ul>
           <li>The Metropolitan Museum of Art: Vincent Van Gogh, <a href="https://www.metmuseum.org/art/collection/search/436535?searchField=All&amp;sortBy=Relevance&amp;high=on&amp;ao=on&amp;showOnly=openAccess&amp;ft=*&amp;offset=280&amp;rpp=40&amp;pos=315"><i>Wheat Field with Cypress</i></a></li>
+          
           <li>Digital Books and Manuscripts: Illustrated capital from the <i>Claricia Psalter</i>, <a href="https://art.thewalters.org/detail/25823/claricia-swinging-on-an-initial-q-2/">Walters MS W.26.64R, fol. 64r</a></li>
+          
           <li>Getty Museum: John Bingley Garland, <a href="https://www.getty.edu/art/collection/object/109Q8D"><i>Collage</i></a></li>
+          
           <li>Online Exhibitions: Nam June Paik, <i>Electronic Superhighway</i> photographed by <a href="https://unsplash.com/photos/xXJ5xPcknRA">Richard Hirajeta</a></li>
         </ul>
       </div>
 
+{/* Met */}
       <div>
         <h2>The Metropolitan Museum of Art</h2>
+        <h3>All images from the Met's collection</h3>
+
+        <ul>
+          <li>John La Farge, <a href="https://www.metmuseum.org/art/collection/search/5582"><i>Peonies Blown in the Wind</i></a></li>
+          
+          <li>Thomas Cole, <a href="https://www.metmuseum.org/art/collection/search/10497"><i>View from Mount Holyoke, Northampton, Massachussettes, after a Thunderstorm -- the Oxbow</i></a></li>
+          
+          <li>Attributed to Exekias, <a href="https://www.metmuseum.org/art/collection/search/250551">Terracotta neck-amphora with lid and knob</a></li>
+          
+          <li>Ogata Kōrin, <a href="https://www.metmuseum.org/art/collection/search/44918"><i>Rough Waves</i></a></li>
+          
+          <li>Vincent Van Gogh, <a href="https://www.metmuseum.org/art/collection/search/436535?searchField=All&amp;sortBy=Relevance&amp;high=on&amp;ao=on&amp;showOnly=openAccess&amp;ft=*&amp;offset=280&amp;rpp=40&amp;pos=315"><i>Wheat Field with Cypress</i></a></li>
+          
+          <li>Edgar Degas, <a href="https://www.metmuseum.org/art/collection/search/438817"><i>The Dance Class</i></a></li>
+          
+          <li>13th-century French workshop, <a href="https://www.metmuseum.org/art/collection/search/469857"><i>Theodosius Arrives at Ephesus</i></a>, from a Scene from the Legend of the Seven Sleepers of Ephesus</li>
+          
+          <li>Moche-Wari <a href="https://www.metmuseum.org/art/collection/search/315786">Tunic</a></li>
+        </ul>
+      </div>
+
+{/* Manuscripts */}
+      <div>
+        <h2>Digital Books and Manuscripts</h2>
           <ul>
-            <li>John La Farge, <a href="https://www.metmuseum.org/art/collection/search/5582"><i>Peonies Blown in the Wind</i></a></li>
-            <li>Thomas Cole, <a href="https://www.metmuseum.org/art/collection/search/10497"><i>View from Mount Holyoke, Northampton, Massachussettes, after a Thunderstorm -- the Oxbow</i></a></li>
-            <li>Attributed to Exekias, <a href="https://www.metmuseum.org/art/collection/search/250551">Terracotta neck-amphora with lid and knob</a></li>
-            <li>Ogata Kōrin, <a href="https://www.metmuseum.org/art/collection/search/44918"><i>Rough Waves</i></a></li>
-            <li>Vincent Van Gogh, <a href="https://www.metmuseum.org/art/collection/search/436535?searchField=All&amp;sortBy=Relevance&amp;high=on&amp;ao=on&amp;showOnly=openAccess&amp;ft=*&amp;offset=280&amp;rpp=40&amp;pos=315"><i>Wheat Field with Cypress</i></a></li>
-            <li>Edgar Degas, <a href="https://www.metmuseum.org/art/collection/search/438817"><i>The Dance Class</i></a></li>
-            <li>13th-century French workshop, <a href="https://www.metmuseum.org/art/collection/search/469857"><i>Theodosius Arrives at Ephesus</i></a>, from a Scene from the Legend of the Seven Sleepers of Ephesus</li>
-            <li>Moche-Wari <a href="https://www.metmuseum.org/art/collection/search/315786">Tunic</a></li>
+            <li><a href="https://unsplash.com/photos/NIJuEQw0RKg">Iñaki del Olmo, </a>Bookshelf</li>
+            <li><i>Book of Kells, </i><a href="https://en.wikipedia.org/wiki/Book_of_Kells#/media/File:KellsFol002rCanonTable.jpg">Canon Table</a></li>
+            
+            <li>Illustrated capital from the <i>Claricia Psalter</i>, <a href="https://art.thewalters.org/detail/25823/claricia-swinging-on-an-initial-q-2/">Walters MS W.26.64R, fol. 64r</a></li>
+            
+            <li>Edwin Davis French, <a href="https://www.metmuseum.org/art/collection/search/821879"><i>Library of the Metropolitan Museum of Art bookplate</i></a></li>
+            
+            <li>Joris Hoefnagel, <i>Mira Calligraphiae Monumenta, </i><a href="https://www.getty.edu/art/collection/object/105TPW">Getty MS 20 (86.MV.527), fol. 143</a></li>
+            
+            <li>13th century French workshop, Dedication page from the <a href="https://www.themorgan.org/manuscript/77422"><i>Moralized Bible of St. Louis</i></a>, Morgan Library MS M.240, fol. 8r</li>
+            <li><a href="https://unsplash.com/photos/2JIvboGLeho">Susan Q Yin, </a>Library shelves from Stockholm, Sweden</li>
+            
+            <li>Georges de Feure, illustration for <a href="https://gallica.bnf.fr/ark:/12148/btv1b8600233m/f194.item"><i>La Porte des rêves</i></a></li>
           </ul>
       </div>
 
-      <div>
-        <h2>Digital Books and Manuscripts</h2>
-      </div>
-
+{/* Getty */}
       <div>
         <h2>Getty Museum</h2>
+        <h3>All images from the Getty's collection</h3>
+
+        <ul>
+          
+          <li>Joris Hoefnagel, <i>Mira Calligraphiae Monumenta, </i><a href="https://www.getty.edu/art/collection/object/105TPW">Getty MS 20 (86.MV.527), fol. 143</a></li>
+          
+          <li>Attributed to Gennaro Laudato, <a href="https://www.getty.edu/art/collection/object/103RS7"><i>Saint Joseph with the Christ Child</i></a></li>
+          
+          <li>Kusakabe Kimbei, <a href="https://www.getty.edu/art/collection/object/108NK7"><i>Woman with Parasol</i></a></li>
+          
+          <li>5th-century Etruscan, <a href="https://www.getty.edu/art/collection/object/103VP7">Statuette of a Kouros</a></li>
+          
+          <li>John Bingley Garland, <a href="https://www.getty.edu/art/collection/object/109Q8D"><i>Collage</i></a></li>
+          
+          <li>T'oros Roslin, <a href="https://www.getty.edu/art/collection/object/107TXD">Canon Table Page</a></li>
+        </ul>
       </div>
 
+{/* Online Exhibitions */}
       <div>
         <h2>Online Exhibitions</h2>
+        <ul>
+          <li>John La Farge, <a href="https://www.metmuseum.org/art/collection/search/5582"><i>Peonies Blown in the Wind</i></a></li>
+          
+          <li>John Bingley Garland, <a href="https://www.getty.edu/art/collection/object/109Q8D"><i>Collage</i></a></li>
+          
+          <li>Pierre-Auguste Renoir, <a href="https://en.wikipedia.org/wiki/Mus%C3%A9e_d%27Orsay#/media/File:Pierre-Auguste_Renoir,_Le_Moulin_de_la_Galette.jpg"><i>Bal du moulin de la Galette</i></a></li>
+
+          <li>Vincent Van Gogh, <a href="https://en.wikipedia.org/wiki/Sunflowers_(Van_Gogh_series)#/media/File:Vincent_van_Gogh_-_Sunflowers_-_VGM_F458.jpg"><i>Sunflowers</i></a></li>
+
+          <li>Sandro Botticelli, <a href="https://en.wikipedia.org/wiki/The_Birth_of_Venus#/media/File:Sandro_Botticelli_-_La_nascita_di_Venere_-_Google_Art_Project_-_edited.jpg"><i>The Birth of Venus</i></a></li>
+
+          <li>Claes Oldenburg, <a href="https://en.wikipedia.org/wiki/File:Shuttlecock_-_Nelson_Art_Gallery.png"><i>Shuttlecock</i></a></li>
+
+          <li>Gilbert Stuart, <a href="https://en.wikipedia.org/wiki/National_Portrait_Gallery_(United_States)#/media/File:Gilbert_Stuart_-_George_Washington_(Lansdowne_Portrait)_-_Google_Art_Project.jpg"><i>The Lansdowne Portrait of George Washington</i></a></li>
+
+          <li>Jan Van Eyck, <a href="https://en.wikipedia.org/wiki/National_Gallery#/media/File:Van_Eyck_-_Arnolfini_Portrait.jpg"><i>The Arnolfini Portrait</i></a></li>
+
+          <li>Rembrandt Van Rijn, <a href="https://en.wikipedia.org/wiki/The_Night_Watch#/media/File:The_Night_Watch_-_HD.jpg"><i>The Night Watch</i></a></li>
+
+          <li>Pieter Brueghel the Elder, <a href="https://en.wikipedia.org/wiki/File:Pieter_Brueghel_the_Elder_-_The_Dutch_Proverbs_-_Google_Art_Project.jpg"><i>The Netherlandish Proverbs</i></a></li>
+
+          <li>Château de Versailles, <a href="https://en.wikipedia.org/wiki/Palace_of_Versailles#/media/File:Chateau_Versailles_Galerie_des_Glaces.jpg">Galerie des Glaces</a></li>
+        </ul>
       </div>
 
+      </div>
 
     </article>
     </>
