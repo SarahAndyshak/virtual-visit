6b8e93b86dd8798a5d5ba2f2c80404f11788bfec
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-09-22 15:09:49 -0700

    Update grid to use buttons to eliminate extra space and link weirdness.

--- src/components/Exhibitions.js
+++ src/components/Exhibitions.js
@@ -32,12 +32,25 @@ function Exhibitions() {
       <h2 className="text-white">Many museums around the world have exhibitions available online, both at their own sites, and on <a className="text-lime-300 hover:text-lime-500" href="https://artsandculture.google.com/partner">Google Arts & Culture.</a> <br /><br />These are just a few suggestions to start your explorations.</h2>
     </div>
 
-
 {/* <Cards */}
     <div className="grid grid-cols-1 gap-1 p-1 items-center md:grid-cols-3">
 
 {/* The Met */}
-      <a className="no-underline" 
+      <div 
+        className="text-center p-20 bg-center bg-cover rounded-lg"
+        style={{backgroundImage: `url(${Window})`}}>
+          <a href="https://artsandculture.google.com/search/exhibit?q=metropolitan%20museum%20of%20art"><Button className="bg-black/75 hover:bg-black/25 text-fuchsia-400 hover:text-white border-none py-1 h-fit text-lg">The Metropolitan Museum of Art</Button></a>
+      </div>
+      
+      {/* just button */}
+      {/* <div 
+        className="text-center bg-center bg-cover rounded-lg"
+        style={{backgroundImage: `url(${Window})`}}>
+          <a href="https://artsandculture.google.com/search/exhibit?q=metropolitan%20museum%20of%20art"><Button className="bg-black/75 hover:bg-black/25 text-fuchsia-400 hover:text-white border-none h-fit p-20 text-lg">The Metropolitan Museum of Art</Button></a>
+      </div> */}
+      
+      {/* original try */}
+      {/* <a className="no-underline" 
         href="https://artsandculture.google.com/search/exhibit?q=metropolitan%20museum%20of%20art">
         <div className="bg-no-repeat bg-cover bg-center rounded-lg"
           style={{backgroundImage: `url(${Window})`}}>
@@ -45,10 +58,16 @@ function Exhibitions() {
             <h2 className="py-8 h-fit text-center text-fuchsia-400 hover:text-white">The Metropolitan Museum of Art</h2>
           </div>
         </div>
-      </a>
+      </a> */}
 
 {/* The Getty */}
-      <a className="no-underline" 
+      <div 
+        className="text-center p-20 bg-center bg-cover rounded-lg"
+        style={{backgroundImage: `url(${CollageGarland})`}}>
+          <a href="https://artsandculture.google.com/search/exhibit?p=the-j-paul-getty-museum"><Button className="bg-black/75 hover:bg-black/25 text-fuchsia-400 hover:text-white border-none py-1 h-fit text-lg">The J. Paul Getty Museum</Button></a>
+      </div>
+
+      {/* <a className="no-underline" 
         href="https://artsandculture.google.com/search/exhibit?p=the-j-paul-getty-museum">
         <div className="bg-no-repeat bg-cover bg-center rounded-lg"
           style={{backgroundImage: `url(${CollageGarland})`}}>
@@ -56,43 +75,16 @@ function Exhibitions() {
             <h2 className="py-8 h-fit text-center text-fuchsia-400 hover:text-white">The J. Paul Getty Museum</h2>
           </div>
         </div>
-      </a>
-
-{/* Musee d'Orsay */}
-      <a className="no-underline" 
-        href="https://artsandculture.google.com/partner/musee-dorsay-paris">
-          <div className="bg-no-repeat bg-cover bg-center rounded-lg"
-            style={{backgroundImage: `url(${Galette})`}}>
-            <div className="bg-black/75 rounded-lg hover:bg-black/25">
-              <h2 className="py-8 h-fit text-center text-fuchsia-400 hover:text-white">Musée d'Orsay</h2>
-            </div>
-        </div>
-      </a>
-
-{/* Van Gogh Museum */}
-      <a className="no-underline" 
-        href="https://artsandculture.google.com/partner/van-gogh-museum">
-        <div className="bg-no-repeat bg-cover bg-center rounded-lg"
-            style={{backgroundImage: `url(${Sunflowers})`}}>
-          <div className="bg-black/75 rounded-lg hover:bg-black/25">
-            <h2 className="py-8 h-fit text-center text-fuchsia-400 hover:text-white">Van Gogh Museum</h2>
-          </div>
-        </div>
-      </a>
-
-{/* Uffizi */}
-      <a className="no-underline" 
-        href="https://artsandculture.google.com/partner/uffizi-gallery">
-        <div className="bg-no-repeat bg-cover bg-top rounded-lg"
-          style={{backgroundImage: `url(${Venus})`}}>
-          <div className="bg-black/75 rounded-lg hover:bg-black/25">
-            <h2 className="py-8 h-fit text-center text-fuchsia-400 hover:text-white">Uffizi Gallery</h2>
-          </div>
-        </div>
-      </a>
+      </a> */}
 
 {/* Nelson-Atkins */}
-      <a className="no-underline" 
+<div 
+        className="text-center p-20 bg-center bg-cover rounded-lg"
+        style={{backgroundImage: `url(${Shuttlecock})`}}>
+          <a href="https://artsandculture.google.com/partner/the-nelson-atkins-museum-of-art"><Button className="bg-black/75 hover:bg-black/25 text-fuchsia-400 hover:text-white border-none py-1 h-fit text-lg">The Nelson-Atkins Museum of Art</Button></a>
+      </div>
+
+      {/* <a className="no-underline" 
         href="https://artsandculture.google.com/partner/the-nelson-atkins-museum-of-art">
         <div className="bg-no-repeat bg-cover bg-center rounded-lg"
           style={{backgroundImage: `url(${Shuttlecock})`}}>
@@ -100,10 +92,67 @@ function Exhibitions() {
             <h2 className="py-8 h-fit text-center text-fuchsia-400 hover:text-white">The Nelson-Atkins Museum of Art</h2>
           </div>
         </div>
-      </a>
+      </a> */}
+
+{/* Musee d'Orsay */}
+      <div 
+        className="text-center p-20 bg-center bg-cover rounded-lg"
+        style={{backgroundImage: `url(${Galette})`}}>
+          <a href="https://artsandculture.google.com/partner/musee-dorsay-paris"><Button className="bg-black/75 hover:bg-black/25 text-fuchsia-400 hover:text-white border-none py-1 h-fit text-lg">Musée d'Orsay</Button></a>
+      </div>
+
+      {/* <a className="no-underline" 
+        href="https://artsandculture.google.com/partner/musee-dorsay-paris">
+          <div className="bg-no-repeat bg-cover bg-center rounded-lg"
+            style={{backgroundImage: `url(${Galette})`}}>
+            <div className="bg-black/75 rounded-lg hover:bg-black/25">
+              <h2 className="py-8 h-fit text-center text-fuchsia-400 hover:text-white">Musée d'Orsay</h2>
+            </div>
+        </div>
+      </a> */}
+
+{/* Van Gogh Museum */}
+      <div 
+        className="text-center p-20 bg-center bg-cover rounded-lg"
+        style={{backgroundImage: `url(${Sunflowers})`}}>
+          <a href="https://artsandculture.google.com/partner/van-gogh-museum"><Button className="bg-black/75 hover:bg-black/25 text-fuchsia-400 hover:text-white border-none py-1 h-fit text-lg">Van Gogh Museum</Button></a>
+      </div>
+
+      {/* <a className="no-underline" 
+        href="https://artsandculture.google.com/partner/van-gogh-museum">
+        <div className="bg-no-repeat bg-cover bg-center rounded-lg"
+            style={{backgroundImage: `url(${Sunflowers})`}}>
+          <div className="bg-black/75 rounded-lg hover:bg-black/25">
+            <h2 className="py-8 h-fit text-center text-fuchsia-400 hover:text-white">Van Gogh Museum</h2>
+          </div>
+        </div>
+      </a> */}
+
+{/* Uffizi */}
+      <div 
+        className="text-center p-20 bg-center bg-cover rounded-lg"
+        style={{backgroundImage: `url(${Venus})`}}>
+          <a href="https://artsandculture.google.com/partner/uffizi-gallery"><Button className="bg-black/75 hover:bg-black/25 text-fuchsia-400 hover:text-white border-none py-1 h-fit text-lg">Uffizi Gallery</Button></a>
+      </div>
+
+      {/* <a className="no-underline" 
+        href="https://artsandculture.google.com/partner/uffizi-gallery">
+        <div className="bg-no-repeat bg-cover bg-top rounded-lg"
+          style={{backgroundImage: `url(${Venus})`}}>
+          <div className="bg-black/75 rounded-lg hover:bg-black/25">
+            <h2 className="py-8 h-fit text-center text-fuchsia-400 hover:text-white">Uffizi Gallery</h2>
+          </div>
+        </div>
+      </a> */}
 
 {/* British Museum */}
-      <a className="no-underline" 
+      <div 
+        className="text-center p-20 bg-center bg-cover rounded-lg"
+        style={{backgroundImage: `url(${BritishMuseum})`}}>
+          <a href="https://artsandculture.google.com/partner/the-british-museum"><Button className="bg-black/75 hover:bg-black/25 text-fuchsia-400 hover:text-white border-none py-1 h-fit text-lg">The British Museum</Button></a>
+      </div>
+
+      {/* <a className="no-underline" 
         href="https://artsandculture.google.com/partner/the-british-museum">
         <div className="bg-no-repeat bg-cover bg-center rounded-lg"
           style={{backgroundImage: `url(${BritishMuseum})`}}>
@@ -111,32 +160,16 @@ function Exhibitions() {
             <h2 className="py-8 h-fit text-center text-fuchsia-400 hover:text-white">The British Museum</h2>
           </div>
         </div>
-      </a>
-
-{/* Smithsonian National Portrait Gallery */}
-      <a className="no-underline" 
-        href="https://artsandculture.google.com/partner/national-portrait-gallery">
-        <div className="bg-no-repeat bg-cover bg-center rounded-lg"
-          style={{backgroundImage: `url(${GeorgeWashington})`}}>
-          <div className="bg-black/75 rounded-lg hover:bg-black/25">
-            <h2 className="py-8 h-fit text-center text-fuchsia-400 hover:text-white">Smithsonian National Portrait Gallery</h2>
-          </div>
-        </div>
-      </a>
-
-{/* The National Gallery, London */}
-      <a className="no-underline" 
-        href="https://artsandculture.google.com/partner/the-national-gallery-london">
-        <div className="bg-no-repeat bg-cover bg-center rounded-lg"
-          style={{backgroundImage: `url(${Arnolfini})`}}>
-          <div className="bg-black/75 rounded-lg hover:bg-black/25">
-            <h2 className="py-8 h-fit text-center text-fuchsia-400 hover:text-white">The National Gallery, London</h2>
-          </div>
-        </div>
-      </a>
+      </a> */}
 
 {/* Rijksmuseum */}
-      <a className="no-underline" 
+      <div 
+        className="text-center p-20 bg-center bg-cover rounded-lg"
+        style={{backgroundImage: `url(${NightWatch})`}}>
+          <a href="https://artsandculture.google.com/partner/rijksmuseum"><Button className="bg-black/75 hover:bg-black/25 text-fuchsia-400 hover:text-white border-none py-1 h-fit text-lg">Rijksmuseum</Button></a>
+      </div>
+
+      {/* <a className="no-underline" 
         href="https://artsandculture.google.com/partner/rijksmuseum">
         <div className="bg-no-repeat bg-cover bg-center rounded-lg"
           style={{backgroundImage: `url(${NightWatch})`}}>
@@ -144,21 +177,16 @@ function Exhibitions() {
             <h2 className="py-8 h-fit text-center text-fuchsia-400 hover:text-white">Rijksmuseum</h2>
           </div>
         </div>
-      </a>
-
-{/* Gemaldegalerie, Staatliche Museen zu Berlin */}
-      <a className="no-underline" 
-        href="https://artsandculture.google.com/partner/gemaldegalerie-staatliche-museen-zu-berlin">
-        <div className="bg-no-repeat bg-cover bg-center rounded-lg"
-          style={{backgroundImage: `url(${NetherlandishProverbs})`}}>
-          <div className="bg-black/75 rounded-lg hover:bg-black/25">
-            <h2 className="py-8 h-fit text-center text-fuchsia-400 hover:text-white">Gemäldegalerie, Staatliche Museen zu Berlin</h2>
-          </div>
-        </div>
-      </a>
+      </a> */}
 
 {/* Versailles */}
-      <a className="no-underline" 
+      <div 
+        className="text-center p-20 bg-center bg-cover rounded-lg"
+        style={{backgroundImage: `url(${Versailles})`}}>
+          <a href="https://artsandculture.google.com/partner/palace-of-versailles"><Button className="bg-black/75 hover:bg-black/25 text-fuchsia-400 hover:text-white border-none py-1 h-fit text-lg">Château de Versailles</Button></a>
+      </div>
+
+      {/* <a className="no-underline" 
         href="https://artsandculture.google.com/partner/palace-of-versailles">
         <div className="bg-no-repeat bg-cover bg-center rounded-lg"
           style={{backgroundImage: `url(${Versailles})`}}>
@@ -166,57 +194,58 @@ function Exhibitions() {
             <h2 className="py-8 h-fit text-center text-fuchsia-400 hover:text-white">Château de Versailles</h2>
           </div>
         </div>
-      </a>
+      </a> */}
 
-
-
-
-
-{/* Test Card */}
-{/* <div className="card bg-red-500 text-primary-content m-1">
-        <div className="card-body">
-          <h2 className="card-title">Card title!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn">Buy Now</button>
-          </div>
-        </div>
-      </div> */}
-
-    {/* <a className="no-underline" href="https://artsandculture.google.com/search/exhibit?q=metropolitan%20museum%20of%20art">
-      <div className="card bg-no-repeat bg-cover bg-center text-primary-content m-1" style={{backgroundImage: `url(${Window})`}}>
-        <div className="card-body items-center text-center">
-          <h1 className="card-title text-white">The Metropolitan Museum of Art</h1>
-        </div>
+{/* The National Gallery, London */}
+      <div 
+        className="text-center p-20 bg-center bg-cover rounded-lg"
+        style={{backgroundImage: `url(${Arnolfini})`}}>
+          <a href="https://artsandculture.google.com/partner/the-national-gallery-london"><Button className="bg-black/75 hover:bg-black/25 text-fuchsia-400 hover:text-white border-none py-1 h-fit text-lg">The National Gallery, London</Button></a>
       </div>
-    </a> */}
 
-{/* Met */}
-      {/* <div className="card bg-no-repeat bg-cover bg-center text-primary-content m-1" style={{backgroundImage: `url(${Window})`}}>
-        <div className="bg-[hsla(0,0%,0%,0.3)]">
-        <div className="flex justify-center items-center h-full">
-          <div className="card-body items-center text-center">
-            <h2 className="card-title text-white">The Met</h2>
-              <Link to="/met"><Button onClick={() => window.scrollTo(0,0)} 
-              className="bg-blue-500 h-fit hover:bg-blue-300">Selected Exhibitions</Button></Link>
-              <a href="https://artsandculture.google.com/search/exhibit?q=metropolitan%20museum%20of%20art"><Button className="bg-blue-500 h-fit hover:bg-blue-300">All Exhibitions</Button></a>
+      {/* <a className="no-underline" 
+        href="https://artsandculture.google.com/partner/the-national-gallery-london">
+        <div className="bg-no-repeat bg-cover bg-center rounded-lg"
+          style={{backgroundImage: `url(${Arnolfini})`}}>
+          <div className="bg-black/75 rounded-lg hover:bg-black/25">
+            <h2 className="py-8 h-fit text-center text-fuchsia-400 hover:text-white">The National Gallery, London</h2>
           </div>
         </div>
-        </div>
-      </div> */}
+      </a> */}
 
-{/* Getty */}
-      {/* <div className="card bg-no-repeat bg-cover bg-center text-primary-content m-1" style={{backgroundImage: `url(${CollageGarland})`}}>
-        <div className="bg-[hsla(0,0%,0%,0.3)]">
-        <div className="flex justify-center items-center h-full">
-          <div className="card-body items-center text-center">
-            <h2 className="card-title text-white">The Getty</h2>
-            <a href="https://www.getty.edu/research/exhibitions_events/online_exhibitions.html"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">The Getty</Button></a>
+{/* Gemaldegalerie, Staatliche Museen zu Berlin */}
+      <div 
+        className="text-center p-20 bg-center bg-cover rounded-lg"
+        style={{backgroundImage: `url(${NetherlandishProverbs})`}}>
+          <a href="https://artsandculture.google.com/partner/gemaldegalerie-staatliche-museen-zu-berlin"><Button className="bg-black/75 hover:bg-black/25 text-fuchsia-400 hover:text-white border-none py-1 h-fit text-lg">Gemäldegalerie, Staatliche Museen zu Berlin</Button></a>
+      </div>
+
+      {/* <a className="no-underline" 
+        href="https://artsandculture.google.com/partner/gemaldegalerie-staatliche-museen-zu-berlin">
+        <div className="bg-no-repeat bg-cover bg-center rounded-lg"
+          style={{backgroundImage: `url(${NetherlandishProverbs})`}}>
+          <div className="bg-black/75 rounded-lg hover:bg-black/25">
+            <h2 className="py-8 h-fit text-center text-fuchsia-400 hover:text-white">Gemäldegalerie, Staatliche Museen zu Berlin</h2>
           </div>
         </div>
-        </div>
-      </div> */}
+      </a> */}
 
+{/* Smithsonian National Portrait Gallery */}
+      <div 
+        className="text-center p-20 bg-center bg-cover rounded-lg"
+        style={{backgroundImage: `url(${GeorgeWashington})`}}>
+          <a href="https://artsandculture.google.com/partner/national-portrait-gallery"><Button className="bg-black/75 hover:bg-black/25 text-fuchsia-400 hover:text-white border-none py-1 h-fit text-lg">Smithsonian National Portrait Gallery</Button></a>
+      </div>
+
+      {/* <a className="no-underline" 
+        href="https://artsandculture.google.com/partner/national-portrait-gallery">
+        <div className="bg-no-repeat bg-cover bg-center rounded-lg"
+          style={{backgroundImage: `url(${GeorgeWashington})`}}>
+          <div className="bg-black/75 rounded-lg hover:bg-black/25">
+            <h2 className="py-8 h-fit text-center text-fuchsia-400 hover:text-white">Smithsonian National Portrait Gallery</h2>
+          </div>
+        </div>
+      </a> */}
 
 {/* Close grid */}
     </div>
@@ -225,6 +254,14 @@ function Exhibitions() {
 
 {/* Close background color */}
     </div>
+
+<div className="grid grid-cols-1 gap-1 items-center my-1 md:grid-cols-3">
+  {/* Eiffel Tower */}
+
+
+</div>
+
+
 
     </article>
     </>
