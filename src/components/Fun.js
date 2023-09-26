e793d7f513e1e11b1e9b74612edf0d41923b9da6
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-09-26 15:34:35 -0700

    Add Tapestry maker image, fix links in Getty.js slideshow

--- src/components/Fun.js
+++ src/components/Fun.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Cats from "./../imgs/KahlerMyWifesLovers.jpeg"
+import Bayeux from "./../imgs/BayeuxExample.png"
 
 
 function Fun() {
@@ -8,8 +9,28 @@ function Fun() {
     <>
     <article className="prose max-w-none">
       <div className="text-center">
-        <h1>Fun Resources</h1>
-        <h2>Just a few of the many fun and creative resources available online.</h2>
+        <h1>Fun Resources!</h1>
+      </div>
+
+{/* Tapestry Maker */}
+      <div>
+        <h2 className="text-center"><a href="https://htck.github.io/bayeux/#!/"> Historic Tale Construction Kit</a></h2>
+        <img className="mx-auto" src={Bayeux} alt="Create your own medieval images with the Historic Tale Construction Kit!" />
+      </div>
+
+{/* Podcasts */}
+      <div>
+        <h2 className="text-center">Podcasts & Videos!</h2>
+
+      </div>
+
+{/* Downloadables */}
+
+{/* Window Swap */}
+
+{/* My Wife's Lovers */}
+      <div>
+        <h2 className="text-center"><a href="https://en.wikipedia.org/wiki/My_Wife%27s_Lovers"><i>My Wife's Lovers,</i> by Carl Kahler</a></h2>
         <img className="mx-auto" src={Cats} alt="19th-century painting by Carl Kahler of 42 cats, entitled My Wife's Lovers"/>
       </div>
     
--- src/components/Getty.js
+++ src/components/Getty.js
@@ -11,11 +11,6 @@ import Kouros from "./../imgs/Getty/GettyKouros.jpg"
 import Parasol from "./../imgs/Getty/KimbeiParasol.jpg"
 import StJoseph from "./../imgs/Getty/LaudatoStJoseph.jpg"
 
-import CanonTableDetail from "./../imgs/Getty/CanonTableDetail.png"
-import KourosDetail from "./../imgs/Getty/KourosDetail.png"
-import JosephDetail from "./../imgs/Getty/JosephDetail.png"
-import ParasolDetail from "./../imgs/Getty/ParasolDetail.png"
-
 
 function Getty() {
   return(
@@ -36,19 +31,10 @@ function Getty() {
           className="bg-cover bg-center w-screen"
           style={{backgroundImage: `url(${Mira})`}}
         >
-          {/* <div className="py-32 text-center">
-            <div>
-              <a href="https://www.getty.edu/art/collection/search?open_content=true"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Open Access Images</Button></a>
-            </div>
-          </div> */}
-          <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <div>
-              <a href="https://www.getty.edu/art/collection/search?open_content=true"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Open Access Images</Button></a>
-            </div>
-          </div>
         </div>
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide6" className="btn btn-circle">❮</a> 
+          <a href="https://www.getty.edu/art/collection/search?open_content=true"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Open Access Images</Button></a>
           <a href="#slide2" className="btn btn-circle">❯</a>
         </div>
       </div> 
@@ -58,14 +44,10 @@ function Getty() {
           className="bg-cover bg-top w-screen"
           style={{backgroundImage: `url(${StJoseph})`}}
         >
-          <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <div>
-              <a href="https://www.getty.edu/research/exhibitions_events/online_exhibitions.html"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Online Exhibitions</Button></a>
-            </div>
-          </div>
         </div>
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide1" className="btn btn-circle">❮</a> 
+          <a href="https://www.getty.edu/research/exhibitions_events/online_exhibitions.html"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Online Exhibitions</Button></a>
           <a href="#slide3" className="btn btn-circle">❯</a>
         </div>
       </div> 
@@ -75,14 +57,10 @@ function Getty() {
           className="bg-cover bg-top w-screen"
           style={{backgroundImage: `url(${Parasol})`}}
         >
-          <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 top-1/2">            
-            <div>
-              <a href="https://www.getty.edu/art/collection/search?department=Manuscripts"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Digitized Manuscripts</Button></a>
-            </div>
-          </div>
         </div>
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide2" className="btn btn-circle">❮</a> 
+          <a href="https://www.getty.edu/art/collection/search?department=Manuscripts"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Digitized Manuscripts</Button></a>
           <a href="#slide4" className="btn btn-circle">❯</a>
         </div>
       </div> 
@@ -92,14 +70,10 @@ function Getty() {
           className="bg-cover bg-top w-screen"
           style={{backgroundImage: `url(${Kouros})`}}
         >
-          <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <div>
-              <a href="https://www.getty.edu/podcasts/"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Podcasts</Button></a>
-            </div>
-          </div>
         </div>        
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide3" className="btn btn-circle">❮</a> 
+          <a href="https://www.getty.edu/podcasts/"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Podcasts</Button></a>
           <a href="#slide5" className="btn btn-circle">❯</a>
         </div>
       </div>
@@ -109,14 +83,10 @@ function Getty() {
           className="bg-cover bg-center w-screen"
           style={{backgroundImage: `url(${CollageGarland})`}}
         >
-          <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <div>
-              <a href="https://www.getty.edu/art/"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Collection Highlights</Button></a>
-            </div>
-          </div>
         </div>
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide4" className="btn btn-circle">❮</a> 
+          <a href="https://www.getty.edu/art/"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Collection Highlights</Button></a>
           <a href="#slide6" className="btn btn-circle">❯</a>
         </div>
       </div>
@@ -126,20 +96,29 @@ function Getty() {
           className="bg-cover bg-center w-screen"
           style={{backgroundImage: `url(${CanonTable})`}}
         >
-          <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <div>
-              <a href="https://artsandculture.google.com/streetview/the-j-paul-getty-museum/cwFdGYSXlaOg6w?sv_lng=-118.473493&sv_lat=34.0771277&sv_h=-18&sv_p=0&sv_pid=SQiOe6lNRxGuLEN-mwxVdA&sv_z=1"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Virtual Walk-through</Button></a>
-            </div>
-          </div>
         </div>
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide5" className="btn btn-circle">❮</a> 
+          <a href="https://artsandculture.google.com/streetview/the-j-paul-getty-museum/cwFdGYSXlaOg6w?sv_lng=-118.473493&sv_lat=34.0771277&sv_h=-18&sv_p=0&sv_pid=SQiOe6lNRxGuLEN-mwxVdA&sv_z=1"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Virtual Walk-through</Button></a>
           <a href="#slide1" className="btn btn-circle">❯</a>
         </div>
       </div>
 
     </div>
 
+{/* Older attempts at slideshow */}
+          {/* <div className="py-32 text-center">
+            <div>
+              <a href="https://www.getty.edu/art/collection/search?open_content=true"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Open Access Images</Button></a>
+            </div>
+          </div> */}
+
+          {/* <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 top-1/2"> */}
+          {/* <div className="flex justify-center">
+            <div>
+              <a href="https://www.getty.edu/art/collection/search?open_content=true"><Button className="bg-rose-900 h-fit hover:bg-rose-700 text-2xl text-amber-300 border-1 border-amber-300 hover:border-amber-300">Open Access Images</Button></a>
+            </div>
+          </div> */}
 
     </article>
     </>
