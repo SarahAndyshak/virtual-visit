ff4c88d46ba1dc7793466ce0c04c8b778fec3c51
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-09-26 12:33:53 -0700

    WIP begin Fun.js

--- src/components/Fun.js
+++ src/components/Fun.js
@@ -1,11 +1,17 @@
 import React from "react";
 
+import Cats from "./../imgs/KahlerMyWifesLovers.jpeg"
+
 
 function Fun() {
   return(
     <>
     <article className="prose max-w-none">
-      
+      <div className="text-center">
+        <h1>Fun Resources</h1>
+        <h2>Just a few of the many fun and creative resources available online.</h2>
+        <img className="mx-auto" src={Cats} alt="19th-century painting by Carl Kahler of 42 cats, entitled My Wife's Lovers"/>
+      </div>
     
     </article>
     </>
